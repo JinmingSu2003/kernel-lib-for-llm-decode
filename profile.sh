f250656929c9145d04baf17e9f37a4ecd72949aa
JinmingSu2003 <sujinming2003@163.com>
2026-09-16 16:13:08 +0800

    change the directory

---
+++ profile.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+project_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+report_dir="$project_dir/src/triton_decode_lab/kernels/ncu-report"
+
+mkdir -p "$report_dir"
+
+report="$1"
+shift
+
+ncu --profile-from-start off --set full -f \
+    -o "$report_dir/$report" "$@"
